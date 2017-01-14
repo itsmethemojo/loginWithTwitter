fc504a6cb81671b87ed08f6cf4f60449faaf149c
itsmethemojo <marianpoeschmann@googlemail.com>
2017-01-14 23:43:20 +0100

    move provisioning scripts in utils repo

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-# cause ubuntu 16.04 does not work nice with vagrant, make sure php7 is available
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y \
-git \
-curl \
-nginx \
-redis-server \
-mongodb-server \
-php7.0 \
-php7.0-fpm \
-php7.0-xml \
-php7.0-zip \
-php7.0-curl \
-php-redis \
-php-mongodb
 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo apt-get install -y curl
 
-# replace nginx default config
-sudo rm /etc/nginx/sites-available/default
-sudo ln -s /vagrant/vagrant/default.conf /etc/nginx/sites-available/default
+#TODO use tag instead of develop
 
-# set up
-sudo rm -rf /usr/share/nginx/html
-sudo ln -s /vagrant/public /usr/share/nginx/html
-
-sudo service nginx restart
-
-# print url to work with
-echo "open http://192.168.100.102/"
+bash <(curl -s https://raw.githubusercontent.com/itsmethemojo/utils/develop/vagrant/provison/php7/webserver.sh)
+bash <(curl -s https://raw.githubusercontent.com/itsmethemojo/utils/develop/vagrant/provison/php7/mongodb.sh)
