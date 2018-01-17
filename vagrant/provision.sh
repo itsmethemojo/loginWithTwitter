c5596f95264b697a08aea3556d4e284ba7904453
itsmethemojo <marianpoeschmann@googlemail.com>
2018-01-17 01:01:31 +0100

    remove vagrant conf

--- vagrant/provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-sudo apt-get install -y curl
-
-#TODO use tag instead of develop
-
-bash <(curl -s https://raw.githubusercontent.com/itsmethemojo/utils/develop/vagrant/provison/php7/webserver.sh)
-bash <(curl -s https://raw.githubusercontent.com/itsmethemojo/utils/develop/vagrant/provison/php7/mongodb.sh)
